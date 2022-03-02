3e190eb18c0514c874fc6c6fc02597dcb3e83bd5
Roberto Gelsi <roberto.gelsi@gkn.com>
2022-03-02 16:01:12 +0100

    Removed unused import java.util.List

--- ranger-presto-plugin-shim/src/main/java/org/apache/ranger/authorization/presto/authorizer/RangerSystemAccessControl.java
+++ ranger-presto-plugin-shim/src/main/java/org/apache/ranger/authorization/presto/authorizer/RangerSystemAccessControl.java
@@ -28,7 +28,6 @@ import org.apache.ranger.plugin.classloader.RangerPluginClassLoader;
 import javax.inject.Inject;
 import java.security.Principal;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
